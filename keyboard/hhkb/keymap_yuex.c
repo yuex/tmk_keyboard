8f7174fb439775cc8c6e56a213513d9f6c948605
yuexin <yuecn41@gmail.com>
2016-09-22 15:33:48 -0400

    update

--- keyboard/hhkb/keymap_yuex.c
+++ keyboard/hhkb/keymap_yuex.c
@@ -61,7 +61,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
     /* Layer 0: Default Layer
      * ,------------------------------------------------------------.
-     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  `|BDel|
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|DEL|BDel|
      * |------------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|     \|
      * |------------------------------------------------------------|
@@ -73,7 +73,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       `-------------------------------------------'
      */
     [0] = \
-    KEYMAP( ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL, GRV,BSPC, \
+    KEYMAP( GRV,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL, DEL,BSPC, \
             TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,BSLS,      \
            LCTL,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT, ENT,           \
            LSFT,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH, FN9,RCTL,           \
@@ -81,22 +81,22 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 
     /* Layer 1: Yuex Layer
      * ,------------------------------------------------------------.
-     * |SLP| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|DEL|BDEL|
+     * |ESC| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|DEL|BDEL|
      * |------------------------------------------------------------|
      * |     |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|PgU|PgD|Insert|
      * |------------------------------------------------------------|
      * |      |HME|WHD|END|   |   |Lft|Dwn| Up|Rgt|F12|  `|    Print|
      * |------------------------------------------------------------|
-     * |        |WHL|WHU|WHR|   |   |   |BT1|AL2|BT2|AL1|      |    |
+     * |        |WHL|WHU|WHR|   |   |   |BT1|AL2|BT2|AL1|      | SLP|
      * `------------------------------------------------------------'
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
     [1] = \
-    KEYMAP(SLEP,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,BSPC, \
+    KEYMAP(ESC ,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,BSPC, \
            TRNS,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,PGUP,PGDN, INS,      \
            TRNS,HOME,WH_D, END,TRNS,TRNS,LEFT,DOWN,  UP,RGHT, F12, GRV,PSCR,           \
-           TRNS,WH_L,WH_U,WH_R,TRNS,TRNS,TRNS,BTN1,ACL2,BTN2,ACL1,TRNS,TRNS,           \
+           TRNS,WH_L,WH_U,WH_R,TRNS,TRNS,TRNS,BTN1,ACL2,BTN2,ACL1,TRNS,SLEP,           \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
     /* Layer #: TRNS */
